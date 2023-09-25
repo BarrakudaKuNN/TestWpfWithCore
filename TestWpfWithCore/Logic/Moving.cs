b7753944c6b3e314e075263fa6a2901fb8602129
Vladislav <Risovanienasaite@gmail.com>
2023-09-25 18:39:35 +0300

    Add project files.

---
+++ TestWpfWithCore/Logic/Moving.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TestWpfWithCore.Logic
+{
+    internal class Moving
+    {
+        Image toMove;
+        public Moving(Image image) 
+        {
+            this.toMove = image;
+        }
+
+        public void Move()
+        {
+            toMove.Margin
+        }
+    }
+}
